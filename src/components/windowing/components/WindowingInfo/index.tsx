ca463ddb55312e7f9dc84f8b13fca609450fc3bd
hanyoung <phanyoung@naver.com>
2024-09-16 20:39:44 +0900

    feat: WindowingInfo toLocaleString 적용

--- src/components/windowing/components/WindowingInfo/index.tsx
+++ src/components/windowing/components/WindowingInfo/index.tsx
@@ -17,14 +17,14 @@ export default function WindowingInfo<T>({
       {createPortal(
         <div className={styles.container}>
           <div>
-            <strong>Total item count:</strong> {data.length}
+            <strong>Total item count:</strong> {data.length.toLocaleString()}
           </div>
           <div>
-            <strong>Currently rendered range:</strong> {start} to {end - 1} (Total:{' '}
+            <strong>Currently rendered range:</strong> {start.toLocaleString()} to {(end - 1).toLocaleString()} (Total:{' '}
             {end - start + 1} items)
           </div>
           <div>
-            <strong>Scroll position (excluding offsetTop):</strong> {scrollOffset}px
+            <strong>Scroll position (excluding offsetTop):</strong> {scrollOffset.toLocaleString()}px
           </div>
         </div>,
         document.body,
